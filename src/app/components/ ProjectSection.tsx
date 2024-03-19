c504842fb918864f152383d4a5f16c5d65e8528f
kobbyMens <starlymaca@StarlyMacas-MacBook-Pro.local>
2024-03-19 08:55:45 +0000

    mobile changes

--- src/app/components/ ProjectSection.tsx
+++ src/app/components/ ProjectSection.tsx
@@ -19,7 +19,8 @@ function ProjectSection() {
 
         <div className="h-full w-full items-center justify-center flex ">
           <div className="w-full h-[70%] md:h-[90%] relative flex overflow-y-hidden overflow-x-scroll snap-x ">
-            <div className="snap-center w-full relative top-[100px] md:top-[165px] selection:flex space-y-6 md:space-y-8 flex-col flex-shrink-0 items-center ">
+            {/*first project */}
+            <div className="snap-center w-full relative top-[85px] md:top-[165px] selection:flex space-y-6 md:space-y-8 flex-col flex-shrink-0 items-center ">
               <div className="project-slide flex items-center flex-col">
                 <Image
                   className="hidden md:block"
@@ -94,22 +95,78 @@ function ProjectSection() {
             </div>
 
             {/* New Project */}
-            <div className="flex snap-center w-full flex-shrink-0 items-center flex-col">
-              <Image
-                className="hidden md:block"
-                src={"/images/projects/survey-app-large-3.png"}
-                width={800}
-                height={700}
-                alt="Survey app image"
-              />
-              <Image
-                className="block md:hidden"
-                src={"/images/projects/survey-app-small.png"}
-                width={300}
-                height={400}
-                alt="Survey app image"
-              />
-              <h3>Project 2/4: FREE SURVEY APP</h3>
+            <div className="snap-center w-full relative top-[85px] md:top-[165px] selection:flex space-y-6 md:space-y-8 flex-col flex-shrink-0 items-center ">
+              <div className="project-slide flex items-center flex-col">
+                <Image
+                  className="hidden md:block"
+                  src={"/images/projects/survey-app-large-3.png"}
+                  width={800}
+                  height={350}
+                  alt="Survey app image"
+                />
+                <Image
+                  className="block md:hidden"
+                  src={"/images/projects/survey-app-small.png"}
+                  width={300}
+                  height={400}
+                  alt="Survey app image"
+                />
+                <h3 className="mt-4 tracking-widest text-base sm:text-lg md:text-xl">
+                  PROJECT 1/4 :{" "}
+                  <TypeAnimation
+                    sequence={[1500, " FREE SURVEY", 500, " "]}
+                    speed={50}
+                    wrapper="span"
+                    repeat={Infinity}
+                  />
+                </h3>
+              </div>
+
+              <div className="libraries-logo-container flex justify-around items-center w-full">
+                <Image
+                  src={"/images/skills-logos/react-js.png"}
+                  className="w-8 h-8 md:w-14 md:h-14"
+                  width={50}
+                  height={50}
+                  alt="react.js logo"
+                />
+                <Image
+                  src={"/images/skills-logos/typescript-logo.webp"}
+                  className="w-8 h-8 md:w-14 md:h-14"
+                  width={50}
+                  height={50}
+                  alt="react.js logo"
+                />
+                <Image
+                  src={"/images/skills-logos/node-js.webp"}
+                  className="w-8 h-8 md:w-14 md:h-14"
+                  width={50}
+                  height={50}
+                  alt="nodejs logo"
+                />
+                <Image
+                  src={"/images/skills-logos/redux.svg"}
+                  className="w-8 h-8 md:w-14 md:h-14"
+                  width={50}
+                  height={50}
+                  alt="redux logo"
+                />
+                <Image
+                  src={"/images/skills-logos/mongoDB-logo.webp"}
+                  className="w-8 h-8 md:w-14 md:h-14"
+                  width={50}
+                  height={50}
+                  alt="mongoDB logo"
+                />
+              </div>
+
+              <p className="project-description text-sm md:text-base lg:text-center text-justify pb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                ac enim vulputate, semper neque eu, facilisis arcu. Phasellus et
+                dui fermentum, pretium urna eget, facilisis nulla. Suspendisse
+                commodo at velit at cursus. Praesent ullamcorper eu enim at
+                viverra
+              </p>
             </div>
           </div>
         </div>
